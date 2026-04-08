0a4046b7168cb69768652737c481edbba190ba25
Reethika Gogireddy <gogireddyreethika1@gmail.com>
2026-04-08 10:46:37 -0700

    Intial Folder Structure

---
+++ frontend/src/App.tsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="App">
+        <h1>Server Log SOC</h1>
+        <p>Welcome to the Server Log Security Operations Center (SOC) dashboard. Here you can monitor and analyze server logs for potential security threats and incidents.</p>
+      </div>
+    </>
+  )
+}
+
+export default App
