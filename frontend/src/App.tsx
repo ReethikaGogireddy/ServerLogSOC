ae29e96c30d2e40c1f0d5beaae3544a25eb99b44
Reethika Gogireddy <gogireddyreethika1@gmail.com>
2026-04-08 12:00:04 -0700

    login setup using clerk

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,4 +1,5 @@
 
+import { SignIn,SignedOut,SignedIn, UserButton } from '@clerk/clerk-react'
 import './App.css'
 
 function App() {
@@ -7,9 +8,26 @@ function App() {
   return (
     <>
       <div className="App">
-        <h1>Server Log SOC</h1>
+
+      <h1>Server Log SOC</h1>
         <p>Welcome to the Server Log Security Operations Center (SOC) dashboard. Here you can monitor and analyze server logs for potential security threats and incidents.</p>
+        
+
+      <SignedOut>
+        <SignIn fallbackRedirectUrl="/dashboard" />
+      </SignedOut>
+
+      
+        
+        <SignedIn>
+        <div className="app-container">
+          <h1>Server Log SOC</h1>
+          <UserButton />
+          <h2>Upload Page</h2>
+        </div>
+      </SignedIn>
       </div>
+      
     </>
   )
 }
